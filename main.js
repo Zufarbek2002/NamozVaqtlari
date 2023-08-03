cbda67eb9c567d7740fdf8c9a754903587d2e5fa
Zufarbek2002 <game123qwerty00@gmail.com>
2023-08-03 19:04:10 +0500

    new comentary

---
+++ main.js
@@ -0,0 +1,28 @@
+
+const bomdod = document.querySelector('.bomdod')
+const quyosh = document.querySelector('.quyosh')
+const peshin = document.querySelector('.peshin')
+const asr = document.querySelector('.asr')
+const shom = document.querySelector('.shom')
+const hufton = document.querySelector('.hufton')
+
+function regionsValue() {
+  let selectedValue = document.querySelector('#regions').value;
+  let API = `https://islomapi.uz/api/present/day?region=${selectedValue}`;
+
+  fetch(API)
+    .then((request) => {
+      return request.json()
+    })
+    .then((data => {
+      bomdod.innerHTML = `Bomdod <br> ${data.times.tong_saharlik}`;
+      quyosh.innerHTML = `Quyosh <br>${data.times.quyosh}`;
+      peshin.innerHTML = `Peshin <br>${data.times.peshin}`;
+      asr.innerHTML = `Asr <br>${data.times.asr}`;
+      shom.innerHTML = `Shom <br>${data.times.shom_iftor}`;
+      hufton.innerHTML = `Hufton <br>${data.times.hufton}`;
+  }))
+  .catch((err) => {console.log(err);})
+  }
+
+ regionsValue()
